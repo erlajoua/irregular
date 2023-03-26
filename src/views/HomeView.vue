cc3cb7577b05adce9dfcb4f22f9da794a8be6ea9
Erwan Lajouannique <erwan.lajouannique@ekimetrics.com>
2023-03-26 14:17:59 +0200

    switch

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
   import Inputs from '@/components/Inputs.vue';
   import Locks from '@/components/Locks.vue';
-
+  import { ref } from 'vue';
+  
+  const showGame = ref(false);
 </script>
 
 <template>
   <div class="w-screen h-screen bg-blue-500">
     <h1 class="bg-red-500">{{ $t('shared.inputs.base') }}</h1>
     <div class="flex items-center justify-center flex-col  gap-2">
-      <Locks />
-      <Inputs />
+      
+      <Locks
+        v-if="!showGame"
+      />
+      <Inputs v-else />
+      <button @click="showGame = !showGame">Switch</button>
     </div>
 
   </div>
