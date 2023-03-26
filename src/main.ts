81ff30e4aa9ab4d743dbae3e0ebc6a466279c763
Erwan Lajouannique <erwan.lajouannique@ekimetrics.com>
2023-03-26 10:39:07 +0200

    i18n merged

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 import router from './router'
 
+import en from './i18n/en.json'
+import fr from './i18n/fr.json'
+
 import './assets/main.css'
+
+const i18n = createI18n({
+	locale: 'en',
+	fallbackLocale: 'en',
+	messages: {
+	  en,
+	  fr
+	}
+});
 
 const app = createApp(App)
 
 app.use(router)
+app.use(i18n)
 
-app.mount('#app')
+app.mount('#app');
